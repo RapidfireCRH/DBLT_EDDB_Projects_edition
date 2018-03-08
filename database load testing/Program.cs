effeb03683a3a87c3de36c831d277da6c8faba5d
RapidfireCRH <Rapidfire882007@gmail.com>
2018-03-07 20:24:39 -0700

    Add project files.

---
+++ database load testing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace database_load_testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
